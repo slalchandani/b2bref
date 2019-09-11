99c85b2aa4de7607de73c8a7bd86520862ca1150
Richard Johnn <rick.johnn@gmail.com>
2019-09-11 16:45:42 -0400

    navbar configuration changes

--- src/layouts/components/TheNavbar.vue
+++ src/layouts/components/TheNavbar.vue
@@ -129,9 +129,9 @@
           <vs-dropdown-menu class="vx-navbar-dropdown">
             <ul style="min-width: 9rem">
               <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"><feather-icon icon="UserIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Profile</span></li>
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"><feather-icon icon="MailIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Inbox</span></li>
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"><feather-icon icon="CheckSquareIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Tasks</span></li>
-              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"><feather-icon icon="MessageSquareIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Chat</span></li>
+              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"><feather-icon icon="AwardIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Account</span></li>
+              <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"><feather-icon icon="SettingsIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Preferences</span></li>
+
               <vs-divider class="m-1"></vs-divider>
               <li class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white" @click="$router.push('/pages/login')"><feather-icon icon="LogOutIcon" svgClasses="w-4 h-4"></feather-icon> <span class="ml-2">Logout</span></li>
             </ul>
